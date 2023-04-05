c1a60653625e598a40bfc409ebcecc999c3d0bf1
unknown <abdi12340.am@gmail.com>
2023-04-05 15:14:45 +0300

    added fpl

--- 2/index.js
+++ 2/index.js
@@ -2,8 +2,8 @@ var width = $(window).width();
 window.onscroll = function(){
 if ((width >= 1000)){
     if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        $("#header").css("background","#fff");
-        $("#header").css("color","#000");
+        $("#header").css("background","#111111");
+        $("#header").css("color","wheat");
         $("#header").css("box-shadow","0px 0px 20px rgba(0,0,0,0.09)");
         $("#header").css("padding","2vh 4vw");
         $("#navigation a").hover(function(){
@@ -15,7 +15,7 @@ if ((width >= 1000)){
         $("#header").css("background","transparent");
         $("#header").css("color","#fff");
         $("#header").css("box-shadow","0px 0px 0px rgba(0,0,0,0)");
-        $("#header").css("padding","6vh 4vw");
+        $("#header").css("padding","6vh");
         $("#navigation a").hover(function(){
             $(this).css("border-bottom","2px solid #fff");
         },function(){
